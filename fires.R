f33201ed8a9fc221a5189e1409f2faf17bd9d475
conner-brew <89354650+conner-brew@users.noreply.github.com>
2021-11-22 08:51:06 -0500

    Add files via upload

---
+++ fires.R
@@ -0,0 +1,32 @@
+#install.packages("forecast")
+library(forecast)
+
+#read data, subset months
+data <- read.csv('forestfires.csv')
+data <- subset(data, data$month %in% c("aug", "sep"))
+
+#Define min-max normalize function
+normalize <- function(x, na.rm = TRUE) {
+  return((x- min(x)) /(max(x)-min(x)))
+}
+
+#Ordinally Encode Days of Week
+days <- factor(data$day, levels = c("mon","tue","wed","thu","fri","sat","sun"))
+data$ordinal_day <- as.integer(days)
+
+#Drop non-numeric columns for modeling
+data <- subset(data, select = -c(month, day))
+
+#transformations in the search for normality
+data$log_area <- normalize(log(data$area + 1))
+data$boxcox_ffmc <- normalize(BoxCox(data$FFMC, lambda = "auto"))
+data$log_isi <- log(data$ISI + 1)
+data$log_RH <- log(data$RH + 1)
+data$log_rain <- log(data$rain + 1)
+
+#Write data
+write.csv(data, "forestfires_cleaned.csv")
+
+#model testing
+model1 <- lm(log_area ~ boxcox_ffmc + log_isi + log_RH + log_rain + wind + temp + DC + DMC + X + Y, data=data)
+model2 <- lm(log_area ~ boxcox_ffmc + ISI + log_RH + log_rain + wind + temp + DC + DMC + X + Y, data=data)
